8e9bb9fcb7349af90daefb47f40be9a40be15cc4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 21:10:35 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,6 +4,7 @@ export default class CoordinatesButton extends React.Component {
   action = (event) => {
     const xPosition = event.clientX
     const yPosition = event.clientY
+    this.props.onReceiveCoordinates([xPosition, yPosition])
   }
   render() {
     return (
